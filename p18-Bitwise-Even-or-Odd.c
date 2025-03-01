1eb8ed113f8b37a5550f2ee06487a3a37d7b5120
badri95699 <badri.gupta.work@gmail.com>
2025-03-01 17:38:02 +0000

    p18

---
+++ p18-Bitwise-Even-or-Odd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    
+    int n;
+    scanf("%d",&n);
+    if((n&1)==0)
+    {
+        printf("Even");
+    }
+    else
+    {
+        printf("Odd");
+    }
+    return 0;
+}
